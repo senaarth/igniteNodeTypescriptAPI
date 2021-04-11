22e81f240cb6bfc689cdbaeb73bc7e27aab05ad3
senaarth <senaarth@gmail.com>
2021-04-11 17:42:49 -0300

    creates routes index file

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,13 @@
 import express from "express";
 import volleyball from "volleyball";
-import { categoriesRouter } from "./routes/categories.routes";
-import { specificationsRouter } from "./routes/specifications.routes";
+import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
 app.use(volleyball);
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello World!" });
-});
-
-app.use("/categories", categoriesRouter);
-app.use("/specifications", specificationsRouter);
+app.use(router);
 
 app.listen("3000", () => {
   console.log("App listenig to port 3000.");
