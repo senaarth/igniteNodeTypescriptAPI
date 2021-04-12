a87ed988d8a3a8cd720679bbe790a9f0a4a9de1a
senaarth <senaarth@gmail.com>
2021-04-11 21:07:41 -0300

    creates categories routes swagger api documentation

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 import volleyball from "volleyball";
+
 import { router } from "./routes";
+import swaggerFile from "../swagger.json";
 
 const app = express();
 
 app.use(express.json());
 app.use(volleyball);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
